808501bd85423c8286148f0c313e3688814360c5
kokoda-yo <angus.li2000@gmail.com>
2021-07-29 18:26:08 +0800

    Added list_item and Availability, edited gradle
    there was a error when I attempted to build the original project. This was solved by changing "android project gradle version" from 7.0.0-beta05 to 4.2.2

---
+++ app/src/main/java/com/r2d2/doctorapp/AvailabilityActivity.java
@@ -0,0 +1,16 @@
+package com.r2d2.doctorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AvailabilityActivity extends AppCompatActivity {
+
+//    ArrayList<String> listOfDates = new ArrayList<String>(new String[]{"2 to 3 pm", "3 to 4 pm"});
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_availability);
+    }
+}
