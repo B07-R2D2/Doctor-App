e1dd89ca1fd8b65a15606fa9c088f010e55f7344
Ian Gregory <justcheesy@outlook.com>
2021-08-02 13:36:11 -0400

    Add "Simulate Patient Login" button to homepage
    We can use this for demos (e.g., tomorrow) until login is complete.

--- app/src/main/java/com/r2d2/doctorapp/HomepageActivity.java
+++ app/src/main/java/com/r2d2/doctorapp/HomepageActivity.java
@@ -1,7 +1,10 @@
 package com.r2d2.doctorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomepageActivity extends AppCompatActivity {
 
@@ -10,4 +13,21 @@ public class HomepageActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
     }
+
+    public void simulatePatientLoginClicked(View view) {
+        Intent intent = new Intent(this, PatientHomeActivity.class);
+        Patient simulatedPatient = new Patient(
+                "Example",
+                "One",
+                "password123",
+                "Male",
+                123456789,
+                "Broken wrist",
+                "example1",
+                1
+        );
+        intent.putExtra(PatientHomeActivity.EXTRA_PATIENT, simulatedPatient);
+        startActivity(intent);
+    }
+
 }
