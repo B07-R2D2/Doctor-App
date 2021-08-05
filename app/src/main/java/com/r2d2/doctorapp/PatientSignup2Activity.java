2a875559621dc36186fda983ad0aa771c011726f
JuefeiLu <juefeilu@gmail.com>
2021-08-05 10:37:26 -0400

    Added Text check
    Makes sure User enters required fields and checks for sin number and gender specifically.

--- app/src/main/java/com/r2d2/doctorapp/PatientSignup2Activity.java
+++ app/src/main/java/com/r2d2/doctorapp/PatientSignup2Activity.java
@@ -9,6 +9,8 @@ import android.widget.EditText;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.regex.Pattern;
+
 public class PatientSignup2Activity extends AppCompatActivity {
     public final DatabaseReference ref = FirebaseDatabase.getInstance().getReference();
     @Override
@@ -20,25 +22,48 @@ public class PatientSignup2Activity extends AppCompatActivity {
     public void signup(View view) {
         // Do something in response to clicking Submit
         Intent backtoLogin = new Intent(this, LoginActivity.class);
+        Pattern genderpattern = Pattern.compile("(male|female|other)");
+        Pattern pattern = Pattern.compile("^[1-9]\\d*");
         EditText send = (EditText) findViewById(R.id.editTextTextMultiLine);
         EditText send2 = (EditText) findViewById(R.id.gender);
         EditText send3 = (EditText) findViewById(R.id.sinNumber);
-        String MEDCON = send.getText().toString();
-        String GEN = send2.getText().toString();
-        String temp = send3.getText().toString();
+        String MEDCON = send.getText().toString().trim();
+        String GEN = send2.getText().toString().trim();
+        String sinString = send3.getText().toString().trim();
         int SIN = 0;
         try {
-            SIN = Integer.parseInt(temp);
+           SIN = Integer.parseInt(sinString);
         }
         catch (NumberFormatException error)
         {
             System.out.println("Could not parse " + error);
+        }
+        if(MEDCON.isEmpty()){
+            send.setError("Please enter your medical condition");
+            send.requestFocus();
+            return;
+        }
+        if(sinString.isEmpty()){
+            send3.setError("Please Enter your SIN Number");
+            send3.requestFocus();
+            return;
+        }
+        else if(pattern.matcher(sinString).matches() == false){
+            send3.setError("SIN Number requires all digits and cannot start with zero");
+            send3.requestFocus();
+            return;
+        }
+        if(GEN.isEmpty()){
+            send2.setError("Please Enter a a short bio about yourself");
+            send2.requestFocus();
+            return;
         }
         Intent get = getIntent();
         String username = get.getStringExtra(PatientSignupActivity.setUSERNAME);
         String password = get.getStringExtra(PatientSignupActivity.setPASSWORD);
         String firstname = get.getStringExtra(PatientSignupActivity.setFIRSTNAME);
         String lastname = get.getStringExtra(PatientSignupActivity.setLASTNAME);
+
         // makes a new patient that is added to database as the constructor in patient does that automatically
         Patient current = new Patient(
                 firstname,
