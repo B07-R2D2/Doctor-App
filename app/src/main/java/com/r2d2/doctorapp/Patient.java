79e0bd260cf3d0f2813c86c0b1475d5e01629138
JuefeiLu <juefeilu@gmail.com>
2021-08-08 17:32:08 -0400

    Updated Patient + Added DOB
    Date of Birth along with more private variables for Patient.

--- app/src/main/java/com/r2d2/doctorapp/Patient.java
+++ app/src/main/java/com/r2d2/doctorapp/Patient.java
@@ -4,6 +4,10 @@ import androidx.annotation.NonNull;
 
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
+
 public class Patient extends User {
 
     public static class Profile extends User.Profile {
@@ -15,12 +19,19 @@ public class Patient extends User {
         private String gender;
         private int sin;
          */
-
         private String medicalCondition = "";
+        private Date DateOfBirth = new Date();
+        private LinkedList<Doctor> DocList = new LinkedList<Doctor>();
+        private LinkedList<Appointment> PastAppointList = new LinkedList<Appointment>();
+        private LinkedList<Appointment> FutureAppointList = new LinkedList<Appointment>();
 
         public String getMedicalCondition() {
             return medicalCondition;
         }
+        public Date getDateOfBirth() {return DateOfBirth;}
+        public LinkedList<Appointment> getPastAppointment() {return PastAppointList;}
+        public LinkedList<Doctor> getDoctorList() {return DocList;}
+        public LinkedList<Appointment> getFutureAppointment() {return FutureAppointList;}
     }
 
     @Override
@@ -48,14 +59,45 @@ public class Patient extends User {
 
     @NonNull
     @Override
-    public String toString() {
+    public String toString()
+    {
         return "Patient " + super.toString();
     }
 
-    public void setMedicalCondition(String med) {
+    public void setMedicalCondition(String med)
+    {
         getProfile().medicalCondition = med;
         pushToDatabase();
     }
 
+    public void setDate(Date d)
+    {
+        getProfile().DateOfBirth = d;
+        pushToDatabase();
+    }
+    public void addPastAppointment(Appointment p)
+    {
+        getProfile().getPastAppointment().add(p);
+    }
+    public void addDoctor(Doctor d)
+    {
+        getProfile().getDoctorList().add(d);
+    }
+    public void addFutureAppointment(Appointment p)
+    {
+        getProfile().getFutureAppointment().add(p);
+    }
+    public void DeletePastAppointment(Appointment p)
+    {
+        getProfile().getPastAppointment().remove(p);
+    }
+    public void DeleteDoctor(Doctor d)
+    {
+        getProfile().getDoctorList().remove(d);
+    }
+    public void DeleteFutureAppointment(Appointment p)
+    {
+        getProfile().getFutureAppointment().remove(p);
+    }
 }
 
---
+++ app/src/test/java/com/r2d2/doctorapp/LoginTest.java
@@ -0,0 +1,13 @@
+package com.r2d2.doctorapp;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+@RunWith(MockitoJUnitRunner.class)
+
+public class LoginTest {
+    @Mock
+    LoginActivity view
+
+}
