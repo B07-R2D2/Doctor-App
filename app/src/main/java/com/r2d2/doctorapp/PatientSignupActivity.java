026a4e2bc79cdfc7bc657e6b58d9a7d1d8d22035
Ian Gregory <justcheesy@outlook.com>
2021-08-09 10:45:02 -0400

    Remove duplicate bugfix

--- app/src/main/java/com/r2d2/doctorapp/PatientSignupActivity.java
+++ app/src/main/java/com/r2d2/doctorapp/PatientSignupActivity.java
@@ -15,7 +15,6 @@ public class PatientSignupActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        presenter = new PatientSignupView(this);
         setContentView(R.layout.activity_patientsignup);
         presenter = new PatientSignupView(this);
     }
