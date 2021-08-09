d54383ee3508ba908e2121c5d8103468a9791db4
JuefeiLu <juefeilu@gmail.com>
2021-08-08 21:37:24 -0400

    Deleted DoctorHomePAge left with basic place holder

--- app/src/main/java/com/r2d2/doctorapp/DoctorHomePageActivity.java
+++ app/src/main/java/com/r2d2/doctorapp/DoctorHomePageActivity.java
@@ -2,9 +2,7 @@ package com.r2d2.doctorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 public class DoctorHomePageActivity extends AppCompatActivity {
 
@@ -12,21 +10,5 @@ public class DoctorHomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_home_page);
-    }
-
-    public void goToCalender(View view)
-    {
-        Intent intent = new Intent(this,DoctorCalendarActivity.class);
-        startActivity(intent);
-    }
-    public void goToHome(View view)
-    {
-        Intent intent = new Intent(this,DoctorHomePageActivity.class);
-        startActivity(intent);
-    }
-    public void goToProfile(View view)
-    {
-        Intent intent = new Intent(this,DoctorProfileActivity.class);
-        startActivity(intent);
     }
 }
