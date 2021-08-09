0b25ee82ffa200166413b8281e60ca72345ba929
Jeremy La <jeremylai511@gmail.com>
2021-08-09 17:29:37 -0400

    Moved register list to doctor profile page.

--- app/src/main/java/com/r2d2/doctorapp/DoctorProfileActivity.java
+++ app/src/main/java/com/r2d2/doctorapp/DoctorProfileActivity.java
@@ -1,7 +1,10 @@
 package com.r2d2.doctorapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -9,15 +12,25 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DoctorProfileActivity extends AppCompatActivity {
     public static final String EXTRA_DOCTOR_PROFILE = "com.r2d2.DoctorApp.DoctorProfileActivity.extra_doctor_profile";
+    private ArrayList<Patient.Profile> patientList = new ArrayList<>();
     private Button deleteButton;
-    private Button registerListButton;
+    private TextView docName;
+    private TextView docGender;
+    private TextView docSpec;
+    private TextView docBio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,17 +40,42 @@ public class DoctorProfileActivity extends AppCompatActivity {
         Intent intent = getIntent();
         Doctor.Profile doctorProfile = (Doctor.Profile) intent.getSerializableExtra(EXTRA_DOCTOR_PROFILE);
 
-        registerListButton = (Button) findViewById(R.id.registerListButton);
-        registerListButton.setOnClickListener(new View.OnClickListener() {
+        // Setting up TextViews for the doctor info
+        docName = findViewById(R.id.doctorProfileName);
+        docGender = findViewById(R.id.doctorProfileGender);
+        docSpec = findViewById(R.id.doctorProfileSpecialization);
+        docBio = findViewById(R.id.doctorProfileBio);
+        docName.setText(doctorProfile.getFirstName() + " " + doctorProfile.getLastName());
+        docGender.setText(doctorProfile.getGender());
+        docSpec.setText(doctorProfile.getSpecialization());
+        docBio.setText(doctorProfile.getBio());
+
+        // Getting past patients from database and initializing the RecyclerView
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Doctors").child(doctorProfile.getUsername()).child("pastPatients");
+        ref.addListenerForSingleValueEvent(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                Log.d("DoctorProfileActivity", "clicked past patients button");
-                Intent intent = new Intent(v.getContext(), RegisterListActivity.class);
-                intent.putExtra(RegisterListActivity.EXTRA_DOCTOR_PROFILE, doctorProfile);
-                v.getContext().startActivity(intent);
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // Iterates over the doctor's past patients
+                for (DataSnapshot child : snapshot.getChildren()){
+                    String patientUsername = child.getValue(String.class);
+                    // probably not the best way to do this, will figure out later
+                    // also not even sure if it works
+                    Patient patient = new Patient(FirebaseDatabase.getInstance(), patientUsername);
+                    patient.addOneTimeObserver(()-> {
+                        patientList.add(patient.getProfile());
+                        initRecyclerView();
+                    });
+                }
+                //initRecyclerView();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
+        // Button to delete the doctor's account
         deleteButton = (Button) findViewById(R.id.doctorDeleteButton);
         deleteButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -83,12 +121,9 @@ public class DoctorProfileActivity extends AppCompatActivity {
         DatabaseReference loginRef = FirebaseDatabase.getInstance().getReference("Doctors").child(username);
         DatabaseReference specRef = FirebaseDatabase.getInstance().getReference("DoctorsSpecial").child(spec).child(username);
 
+        // Removing from database
         loginRef.removeValue();
         specRef.removeValue();
-        // TODO: figure out why it does the following
-        // if there are multiple doctors in the same specialization branch, it deletes one doctor only
-        // if there is only one doctor, it deletes the doctor and the specialization branch
-        // not sure why it does this, but it sure makes the job a lot easier
 
         Toast.makeText(this, "Deleting " + username, Toast.LENGTH_SHORT).show();
 
@@ -97,5 +132,12 @@ public class DoctorProfileActivity extends AppCompatActivity {
         //Send user back to log in page.
         Intent intent = new Intent(this,LoginActivity.class);
         startActivity(intent);
+    }
+
+    private void initRecyclerView(){
+        RecyclerView recyclerView = findViewById(R.id.registerListRecycler);
+        RecyclerViewAdapter2 adapter = new RecyclerViewAdapter2(this, patientList);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
