30acd5fba180676f8bdbad4d580dbc8e89ff5b0c
Jeremy La <jeremylai511@gmail.com>
2021-08-11 03:13:14 -0400

    added splash screen
    fade out doesn't work :(

---
+++ app/src/main/java/com/r2d2/doctorapp/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.r2d2.doctorapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread splash = new Thread(){
+            @Override
+            public void run() {
+                try{
+                    sleep(4*1000);
+                    Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+                }
+                catch (InterruptedException e){
+
+                }
+            }
+        };
+        splash.start();
+    }
+}
