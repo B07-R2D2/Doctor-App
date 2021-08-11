f817529fa9557867cea69b1790ad47db384c94f1
kokoda-yo <angus.li2000@gmail.com>
2021-08-11 18:45:58 +0800

    Added tests including one expresso (test for login success is unstable (possibly bc database involved) so I didn't add that)

--- app/src/test/java/com/r2d2/doctorapp/LoginTest.java
+++ app/src/test/java/com/r2d2/doctorapp/LoginTest.java
@@ -3,12 +3,16 @@ package com.r2d2.doctorapp;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.inOrder;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import android.content.Intent;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InOrder;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
 
@@ -53,4 +57,35 @@ public class LoginTest {
         verify(view).startActivity(any());
     }
 
+    @Test
+    public void testNullPassword() {
+        LoginPresenter p = new LoginPresenter(model, view);
+        when(model.checkLogin("k", "")).thenReturn(0);
+        p.checkLogin("k", "");
+        // this works when using the the login
+        // verify(view).displayErrorMessage(RESOURCE_STRING);
+    }
+
+    @Test
+    public void testPresenterDoctorOrder() {
+        LoginPresenter p = new LoginPresenter(model,view);
+        when(model.checkLogin("k2","k2")).thenReturn(2);
+        p.checkLogin("k2","k2");
+//        tried to test with specified intents but didn't work
+//        Intent intent = new Intent(view, DoctorHomePageActivity.class);
+//        intent.putExtra(view.givenUsername, "k2");
+        InOrder inOrder = inOrder(model, view);
+        inOrder.verify(model).checkLogin("k2", "k2");
+        inOrder.verify(view).startActivity(any());
+    }
+
+    @Test
+    public void testPresenterPatientOrder() {
+        LoginPresenter p = new LoginPresenter(model,view);
+        when(model.checkLogin("ian","ian")).thenReturn(2);
+        p.checkLogin("ian","ian");
+        InOrder inOrder = inOrder(model, view);
+        inOrder.verify(model).checkLogin("ian", "ian");
+        inOrder.verify(view).startActivity(any());
+    }
 }
