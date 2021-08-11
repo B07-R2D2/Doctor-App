57f7776a6abb70e437b44f391be9330426f375dd
Jeremy La <jeremylai511@gmail.com>
2021-08-11 03:40:20 -0400

    click anywhere

--- app/src/main/java/com/r2d2/doctorapp/SplashActivity.java
+++ app/src/main/java/com/r2d2/doctorapp/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.r2d2.doctorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -14,23 +15,34 @@ public class SplashActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
-        Thread splash = new Thread(){
-            @Override
-            public void run() {
-                try{
-                    sleep(4*1000);
-                    //finish(); //if you put finish() here, the transition looks different, no idea why
-                    Intent intent = new Intent(getBaseContext(), LoginActivity.class);
-                    startActivity(intent);
-                    finish();
-                    // this doesn't work :(
-                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-                }
-                catch (InterruptedException e){
+//        Thread splash = new Thread(){
+//            @Override
+//            public void run() {
+//                try{
+//                    sleep(4*1000);
+//                    //finish(); //if you put finish() here, the transition looks different, no idea why
+//                    Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+//                    startActivity(intent);
+//                    finish();
+//                    // this doesn't work :(
+//                    overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+//                }
+//                catch (InterruptedException e){
+//
+//                }
+//            }
+//        };
+//        splash.start();
 
-                }
+        ConstraintLayout layout = (ConstraintLayout) findViewById(R.id.splashLayout);
+        layout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+                startActivity(intent);
+                finish();
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             }
-        };
-        splash.start();
+        });
     }
 }
